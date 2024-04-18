db952a43f232dd8fccaea4b0e8cbd65462daac89
Taylor Moreira <taylormoreira1@gmail.com>
2024-04-18 13:51:13 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Task from "./pages/Tasks";
+import Register from "./pages/Register";
+import { RouteAuthorization } from "./components/RouteAuthorization";
 
 export default function App() {
-
   return (
-    <div>
-   
-        
-    </div >
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/tarefas" element={
+            <RouteAuthorization>
+              <Task />
+            </RouteAuthorization>}
+          />
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
